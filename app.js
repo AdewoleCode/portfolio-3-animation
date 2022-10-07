ad8d5bc472c031e79809e61dfc3176a2569a6ae9
ademola <adeadewole1712@gmail.com>
2022-10-07 12:21:32 +0100

    finished

---
+++ app.js
@@ -0,0 +1,12 @@
+const links = document.querySelectorAll('.navbar ul li a');
+
+for (const link of links){
+    link.addEventListener('click', smoothScroll);
+}
+
+
+function smoothScroll(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    document.querySelector(href).scrollIntoView(({behavior: "smooth"}));
+}
